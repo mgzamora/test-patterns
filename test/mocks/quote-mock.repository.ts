9ed29d5f2aa35f3d640cb0f21656dc875e77f19d
Manuel Gutiérrez <mg@bnext.es>
2020-12-30 17:07:39 +0100

    added quote mock repository implementation for testing purposes

--- test/mocks/quote-mock.repository.ts
+++ test/mocks/quote-mock.repository.ts
@@ -2,10 +2,9 @@ import { QuoteRepository } from '../../src/domain/quote.repository';
 import { Quote } from '../../src/domain/quote';
 import { QuoteId } from '../../src/domain/quote-id';
 
-export class QuoteRepositoryMock implements QuoteRepository {
+export class QuoteMockRepository implements QuoteRepository {
     private mockSave = jest.fn();
     private mockSearch = jest.fn();
-    private quote: Quote = null;
     private quotes: Quote[] = [];
     
     save(quote: Quote): Promise<void> {
