9347c2cf910021a66270df9b0206d345c0a69610
Manuel Gutiérrez <mg@bnext.es>
2020-12-30 14:12:10 +0100

    update lass name

--- src/infrastructure/rest/rest.module.ts
+++ src/infrastructure/rest/rest.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER } from '@nestjs/core';
 import { ProxyServicesDynamicModule } from '../use-case-proxy/proxy-services-dynamic.module';
-import { QuoteController } from './quote.controller';
 import { InvalidArgumentErrorFilter } from './filters/invalid-argument-error.filter';
 import { InvalidQuoteErrorFilter } from './filters/invalid-quote-error.filter';
 import { MainController } from './main.controller';
+import { QuoteGetController } from './quote-get.controller';
 
 @Module({
   imports: [ProxyServicesDynamicModule.register()],
-  controllers: [MainController, QuoteController],
+  controllers: [MainController, QuoteGetController],
   providers: [{ provide: APP_FILTER, useClass: InvalidQuoteErrorFilter  },
       { provide: APP_FILTER, useClass: InvalidArgumentErrorFilter }],
 })
